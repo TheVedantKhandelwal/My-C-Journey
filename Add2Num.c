a785918788072d3f14796f53eb4c2ffdbe47fcec
TheVedantKhandelwal <57403056+TheVedantKhandelwal@users.noreply.github.com>
2023-02-08 01:15:06 +0530

    Create Add2Num.c

---
+++ Add2Num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Code To input two numbers from user and print the sum of them
+void main()
+{
+    int a,b,c;
+    printf("Enter the Value of A");
+    scanf("%d",&a);
+    printf("Enter the Value of B");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum of %d and %d is %d",a,b,c);
+}
